278859be0b61b81b1a98c1c9585c267f6ef0c47c
Audrey <audreyjo35@gmail.com>
2022-02-08 22:24:33 +0900

    create the intent for when user click send text button

--- app/src/main/java/com/ebookfrenzy/explicitintent/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/explicitintent/MainActivity.java
@@ -2,6 +2,7 @@ package com.ebookfrenzy.explicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         super.onCreate(savedInstanceState);
         setContentView(view);
+    }
+
+    public void sendText(View view) {
+
+        // new Intent instance created, pass current activity (this) and SecondActivity
+        Intent i = new Intent(this, SecondActivity.class);
+
+        // extract text entered into EditText1
+        String myString = binding.editText1.getText().toString();
+
+        // extracted text is added to the intent object as key-value pair
+        i.putExtra("qString", myString);
+
+        // intent is started as the intent object is passed (the one containing extracted text)
+        startActivity(i);
     }
 }

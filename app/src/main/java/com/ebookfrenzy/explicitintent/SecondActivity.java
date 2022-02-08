1da77d919219b8176e1d8411931042c6c8433bc7
Audrey <audreyjo35@gmail.com>
2022-02-08 22:34:14 +0900

    extract intent data and display in 2nd activity

--- app/src/main/java/com/ebookfrenzy/explicitintent/SecondActivity.java
+++ app/src/main/java/com/ebookfrenzy/explicitintent/SecondActivity.java
@@ -3,12 +3,30 @@ package com.ebookfrenzy.explicitintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ebookfrenzy.explicitintent.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ActivitySecondBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+
+        // extract qString from the intent object
+        String qString = extras.getString("qString");
+        binding.textView2.setText(qString);
     }
 }

e63e409258809d78856d0bc492ad46360428316a
Audrey <audreyjo35@gmail.com>
2022-02-08 19:38:40 +0900

    migrate a project to view binding
    - avoid using + in version numbers can lead to unpredictable and unrepeatable builds
    - accept the recommended action (remove + sign)
    - this removes the suggestions appearing in
    File > Project Structure
    - https://stackoverflow.com/a/46794087

--- app/src/main/java/com/ebookfrenzy/explicitintent/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/explicitintent/MainActivity.java
@@ -3,12 +3,19 @@ package com.ebookfrenzy.explicitintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ebookfrenzy.explicitintent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(view);
     }
 }

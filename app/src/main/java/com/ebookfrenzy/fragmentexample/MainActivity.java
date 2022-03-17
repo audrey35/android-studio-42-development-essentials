575f8c4ebb2ae446cf0e160de593de6675864eb9
Audrey Jo <audreyjo35@gmail.com>
2022-03-17 16:07:24 +0900

    migrate the project to view binding

--- app/src/main/java/com/ebookfrenzy/fragmentexample/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/fragmentexample/MainActivity.java
@@ -3,12 +3,19 @@ package com.ebookfrenzy.fragmentexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ebookfrenzy.fragmentexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
         setContentView(R.layout.activity_main);
     }
 }

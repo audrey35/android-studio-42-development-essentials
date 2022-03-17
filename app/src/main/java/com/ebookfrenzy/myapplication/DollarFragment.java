31e28fbb68995d99e1ab04b018a87f9162f596c1
Audrey Jo <audreyjo35@gmail.com>
2022-03-18 07:41:43 +0900

    migrate dollar fragment to view binding

--- app/src/main/java/com/ebookfrenzy/myapplication/DollarFragment.java
+++ app/src/main/java/com/ebookfrenzy/myapplication/DollarFragment.java
@@ -2,11 +2,14 @@ package com.ebookfrenzy.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.ebookfrenzy.myapplication.databinding.FragmentDollarBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DollarFragment extends Fragment {
+
+    private FragmentDollarBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,9 +61,16 @@ public class DollarFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dollar, container, false);
+        binding = FragmentDollarBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }

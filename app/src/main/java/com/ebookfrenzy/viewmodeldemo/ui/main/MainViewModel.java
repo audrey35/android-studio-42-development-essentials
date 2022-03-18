d9dbd13fe1caef909381f79dc26c909c76ba4efb
Audrey Jo <audreyjo35@gmail.com>
2022-03-18 16:58:59 +0900

    add LiveData to the view model

--- app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
+++ app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
@@ -1,20 +1,25 @@
 package com.ebookfrenzy.viewmodeldemo.ui.main;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
     private static final Float rate = 0.74F;
     private String dollarText = "";
-    private Float result = 0F;
 
+    // change result to be mutable live data
+    private MutableLiveData<Float> result = new MutableLiveData<>();
+
+
+    // use setValue rather than assignment operator for result
     public void setAmount(String value) {
         this.dollarText = value;
-        result = Float.parseFloat(dollarText)*rate;
+        result.setValue(Float.parseFloat(dollarText)*rate);
     }
 
-    public Float getResult()
-    {
+    // now returns MutableLiveData<Float> rather than returning Float
+    public MutableLiveData<Float> getResult() {
         return result;
     }
 }
